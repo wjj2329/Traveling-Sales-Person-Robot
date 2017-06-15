196b9c9d9683b66ed1178163300b6efb3bfd0356
MasqueradeOfSilence <alexcn711@gmail.com>
2017-06-14 21:51:31 -0600

    method stubs

--- src/RobotFunctions/MachineVision.java
+++ src/RobotFunctions/MachineVision.java
@@ -1,5 +1,9 @@
 package RobotFunctions;
 
+import Map.Node;
+
+import java.util.ArrayList;
+
 /**
  * Created by Alex on 6/14/17.
  * Class designed to encapsulate the machine vision,
@@ -7,5 +11,30 @@ package RobotFunctions;
  */
 public class MachineVision
 {
+    /**
+     * The two robots will divide up the grid in half.
+     * Each will use a simulated annealing approach
+     *  to compute a path to each of the "cities",
+     *  implemented as arUco cards.
+     * @return path
+     */
+    public ArrayList<Node> computeTSP()
+    {
+        return null;
+    }
 
+    /**
+     * Similar to the computeTSP() method,
+     *  only the robots will move in pretty arcs,
+     *  like two graceful trains.  ☆ﾟ°˖* ᕕ( ᐛ )ᕗ
+     *
+     * But we do need a practical application for my good ol' homedog Dr. Seppi.
+     *  Working on that.
+     *  DONUTS!
+     * @return path
+     */
+    public ArrayList<Node> computePrettyTSP()
+    {
+        return null;
+    }
 }
